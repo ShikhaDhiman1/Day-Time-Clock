22de9b1253128d688676dedb62bfbe6babd3b32c
Shikha Dhiman <dshikha4403@gmail.com>
2022-08-23 22:57:15 +0530

    current date and time using javascript

---
+++ interactClock.js
@@ -0,0 +1,19 @@
+function dayDateTimer(){
+    let currdd = new Date();
+    let hh = currdd.getHours();
+    let mm = currdd.getMinutes();
+    let ss = currdd.getSeconds();
+
+    let day = currdd.getDate();
+    let month = currdd.getMonth();
+    let year = currdd.getFullYear();
+
+    const date = currdd.getDate();
+
+    // document.getElementById("date-time-clock").value = `${hh}:${mm}:${ss} \n\ ${day}-${month}-${year}`;
+   
+    // console.log(hh + ":" + mm +":" + ss +"<br>"+day+"-"+month+"-"+year);
+    document.getElementById("date-time-clock").value = hh + ":" + mm +":"
+             + ss +"\n"+day+"-"+month+"-"+year; 
+}
+setInterval(dayDateTimer, 1000);
